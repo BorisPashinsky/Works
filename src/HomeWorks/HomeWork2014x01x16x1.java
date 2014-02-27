c710f01f6645d95dd67f7c740e8e7d43b3d7c8e8
Boris Pashinsky <boris.pashinsky@gmail.com>
2014-02-27 22:17:57 +0400

    Изменена сортировка за O(n^2)

--- src/HomeWorks/HomeWork2014x01x16x1.java
+++ src/HomeWorks/HomeWork2014x01x16x1.java
@@ -3,23 +3,38 @@ package HomeWorks;
 import java.util.Scanner;
 
 public class HomeWork2014x01x16x1 {
-	public static double[] f(double[] a, double[] b) {
+	public static int[] f(int[] a, int[] b) {
 		if(a.length < b.length){
-			int k = a.length;int s = 0;
-			for(int i = 0; (i < k)&&(b[k]>a[i]);i++){
-				s = i;
-			}
-			double[] c = new double[k+1];
-			for(int i = 0; i < k+1;i++){
-				if(i < s){
-					c[i] = a[i];
-				}
-				if(i == s){
-					c[i] = b[k];
-				}if(i > s){
+			int t = b[a.length];
+			int[] c = new int[a.length + 1];
+			boolean bu = true;
+			for(int i = 0;(i < a.length + 1); i++){
+				if(bu){
+					if(i < a.length? (t > a[i]):false){
+						c[i] = a[i];
+					}else{
+						c[i] = t;
+						bu = false;
+					}
+				}else{
 					c[i] = a[i - 1];
 				}
 			}
+			//			int k = a.length;int s = 0;
+			//			for(int i = 0; (i < k)&&(b[k]>a[i]);i++){
+			//				s = i;
+			//			}
+			//			double[] c = new double[k+1];
+			//			for(int i = 0; i < k+1;i++){
+			//				if(i < s+1){
+			//					c[i] = a[i];
+			//				}
+			//				if(i == s + 1){
+			//					c[i] = b[k];
+			//				}if(i > s + 1){
+			//					c[i] = a[i - 1];
+			//				}
+			//			}
 			return f(c,b);
 		}else{
 			return a;
@@ -29,19 +44,17 @@ public class HomeWork2014x01x16x1 {
 	public static void main(String[] args) {
 		int n = 5;
 		Scanner im = new Scanner(System.in);
-		double[] a = new  double[n];
+		int[] a = new  int[n];
 		for(int i = 0; i < n; i++){
-			a[i] = im.nextDouble();
+			a[i] = im.nextInt();
 		}
-		double[] f = new  double[n];
-		double[] e = new double[1];
+		int[] e = new int[1];
 		e[0] = a[0];
-		f = f(e,a);
+		a = f(e,a);
 		for(int i = 0; i < n;i++){
-			System.out.println(f[i]);
+			System.out.println(a[i]);
 		}
 		im.close();
-
 	}
 
 }
